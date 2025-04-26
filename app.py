d794cf205ba63294022467e2765a255ea8d26c8a
omkardhuri123 <103624757+omkardhuri123@users.noreply.github.com>
2025-04-26 19:03:55 +0530

    app.py

--- app.py
+++ app.py
@@ -6,16 +6,15 @@ from dotenv import load_dotenv
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 from waitress import serve
-from extensions import init_extensions,db, migrate, freezer
+from extensions import init_extensions,freezer
 
 
 # --- Environment Setup ---
 load_dotenv()
 
 # Initialize extensions
-db.init_app(app)
-migrate.init_app(app, db)
 freezer.init_app(app)  # Initialize Frozen-Flask
+
 # --- Logging Configuration ---
 logging.basicConfig(
     level=logging.INFO,
