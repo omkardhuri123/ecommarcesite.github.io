c9f312d4821e953489e37d47b65301ea812ef957
omkardhuri123 <dhuri8973@gmail.com>
2025-04-27 00:04:54 +0530

    Add database migrations environment configuration

---
+++ migrations/env.py
@@ -0,0 +1,47 @@
+from __future__ import with_statement
+
+import logging
+from logging.config import fileConfig
+
+from flask import current_app
+
+from alembic import context
+
+# this is the Alembic Config object
+config = context.config
+
+# Interpret the config file for Python logging
+fileConfig(config.config_file_name)
+logger = logging.getLogger('alembic.env')
+
+def get_url():
+    return str(current_app.extensions['migrate'].db.get_engine().url).replace('%', '%%')
+
+config.set_main_option('sqlalchemy.url', get_url())
+target_metadata = current_app.extensions['migrate'].db.metadata
+
+def run_migrations_offline():
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url, target_metadata=target_metadata, literal_binds=True
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+def run_migrations_online():
+    connectable = current_app.extensions['migrate'].db.get_engine()
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
