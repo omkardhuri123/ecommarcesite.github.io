e96e910ed7bb8873bc11625b40c68eee4306da2d
omkardhuri123 <dhuri8973@gmail.com>
2025-04-26 20:25:23 +0530

    Fix SQL syntax in database.sql

--- database.sql
+++ database.sql
@@ -19,15 +19,22 @@ USE `kokani_bazaar`;
 
 --
 -- Table structure for table `alembic_version`
---
 
+--
+-- Table structure for table `alembic_version`
 DROP TABLE IF EXISTS `alembic_version`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `alembic_version` (
   `version_num` varchar(32) NOT NULL,
   PRIMARY KEY (`version_num`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*DROP TABLE IF EXISTS `alembic_version`;*/
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*CREATE TABLE `alembic_version` (
+  `version_num` varchar(32) NOT NULL,
+  PRIMARY KEY (`version_num`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;*/
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
