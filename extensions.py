c396de6635fac651ddcf2d9135c262135cc739dd
omkardhuri123 <103624757+omkardhuri123@users.noreply.github.com>
2025-04-26 19:02:49 +0530

    extensions.py

--- extensions.py
+++ extensions.py
@@ -1,9 +1,11 @@
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_frozen import Freezer
 
 db = SQLAlchemy()
 migrate = Migrate()
+freezer = Freezer()
 
 def init_extensions(app):
     db.init_app(app)
